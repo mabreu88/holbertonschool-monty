09819d989c53f807fc07b4e63cb6626bb042c439
Martin <martymalkav7@gmail.com>
2023-08-31 00:01:05 +0000

    pall function

---
+++ pall.c
@@ -0,0 +1,21 @@
+#include "monty.h"
+/**
+ * pall - print all the values of the stack
+ * @stack: double pointer to the stack
+ * @n: data to print
+ * Return: nothing
+ */
+
+void pall(stack_t **stack, unsigned int n)
+{
+	stack_t *current = NULL;
+	(void)n;
+
+	current = *stack;
+
+	while (current != NULL)
+	{
+		dprintf(STDOUT_FILENO, "%d\n", current->n);
+		current = current->next;
+	}
+}
