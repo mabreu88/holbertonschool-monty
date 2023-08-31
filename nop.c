cd1022ee29b2e78265d12b8d19b2ce0c3b6b6a86
Martin <martymalkav7@gmail.com>
2023-08-31 00:30:19 +0000

    nop function

---
+++ nop.c
@@ -0,0 +1,11 @@
+#include "monty.h"
+/**
+ *nop - doesn’t do anything.
+ *@stack: double pointer to head of stack
+ *@line_number: line number of current operation
+*/
+void nop(stack_t **stack, unsigned int line_number)
+{
+	(void)stack;
+	(void)line_number;
+}
