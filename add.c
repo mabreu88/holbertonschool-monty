ed220e98fc6e4e6c5e9c1b7a0a8daeb1e1b986a5
Martin <martymalkav7@gmail.com>
2023-08-31 00:29:12 +0000

    add function

---
+++ add.c
@@ -0,0 +1,31 @@
+#include "monty.h"
+/**
+ *add - adds the top two elements of the stack
+ *@stack: stack storing data
+ *@line_number: line of the command
+ *Return: nothing
+ */
+void add(stack_t **stack, unsigned int line_number)
+{
+	int i = 0, sum = 0;
+	stack_t *temp;
+
+	temp = *stack;
+	while (temp)
+	{
+		temp = temp->next;
+		i++;
+	}
+	if (i < 2)
+	{
+		dprintf(STDERR_FILENO, "L%u: can't add, stack too short\n", line_number);
+		fclose(buffer.fd);
+		free(buffer.line);
+		free(temp);
+		exit(EXIT_FAILURE);
+	}
+	temp = *stack;
+	sum = temp->n + temp->next->n;
+	temp->next->n = sum;
+	_pop(stack, line_number);
+}
