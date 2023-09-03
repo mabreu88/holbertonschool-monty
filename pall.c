db04a9869936677172b566a8adf59815bb89a9e7
Diego Piñeyro <124789249+Diego29012@users.noreply.github.com>
2023-09-03 19:48:17 -0300

    Update pall.c

--- pall.c
+++ pall.c
@@ -5,17 +5,16 @@
  * @n: data to print
  * Return: nothing
  */
-
 void pall(stack_t **stack, unsigned int n)
 {
-	stack_t *current = NULL;
-	(void)n;
+    stack_t *current = NULL;
+    (void)n;
 
-	current = *stack;
+    current = *stack;
 
-	while (current != NULL)
-	{
-		dprintf(STDOUT_FILENO, "%d\n", current->n);
-		current = current->next;
-	}
+    while (current != NULL)
+    {
+        dprintf(STDOUT_FILENO, "%d\n", current->n);
+        current = current->next;
+    }
 }
